d6542af2147828b26c8647a5131b35a66c67f6ab
Medhir05 <medhirgulati@gmail.com>
2024-11-09 11:55:35 +0000

    Improvements to code
    Today I fixed two problems:
    
    1. The user could not input an exercise name which was more than one word. To fix this I implemented std::getline.
    
    2. The user could not input exercise information on more than 1 exercise. To fix this I needed to alter the functionality of main.cpp. I implemented two for loops and also an array. This fixed the issue.
    
    I also began adding comments to my code to improve readability.

--- Exercises.cpp
+++ Exercises.cpp
@@ -1,6 +1,7 @@
 #include "Exercises.h"
 #include <iostream>
 #include <string> 
+#include <vector>
 
 Exercises::Exercises()
 {
@@ -47,25 +48,3 @@ void Exercises::repsNum(int amount)
 
 
 
-/*
-void Exercises::getExerciseDetails() {
-    int numExercises;
-    std::cout << "Enter number of exercises: \n"; 
-    std::cin >> numExercises; 
-
-    for (int i = 0; i < numExercises; ++i){
-        Exercises exercise; 
-
-        std::cout << "Enter the name of exercise " << i+1 << " :\n"; 
-        std::getline(std::cin , exercise.name); 
-
-        std::cout << "Enter the number of sets for " << exercise.name << " :\n"; 
-        std::cin >> exercise.sets; 
-
-        std::cout << "Enter the number of reps per set for " << exercise.name << " :\n"; 
-        std::cin >> exercise.reps;
-      
-    void Exercises::displayWorkout() {
-    std::cout << "\nYour Exercise Plan:\n";
-    }
-*/
--- main.cpp
+++ main.cpp
@@ -1,27 +1,44 @@
 #include "Exercises.h"
+#include <iostream> 
+#include <string> 
+#include <vector> 
 
 int main()
 {
-    int numExercise; 
-    std::string nameExercise; 
-    int numSets; 
-    int numReps; 
+    int numExercise; // Variable for number of exercises the user chooses
+    Exercises a; // First class overload 
+    std::cout << "Enter the amount of exercises in your routine: "; // Asking user how many exercises they want 
+    std::cin >> numExercise; // Storing value 
+    std::cin.ignore(); // Avoids new line issues 
 
-    Exercises a;
-    std::cout << "Enter the amount of exercises in your routine: "; 
-    std::cin >> numExercise; 
-    std::cout << "Enter the name of the exercise: "; 
-    std::cin >> nameExercise; 
-    std::cout << "Enter number of sets: ";
-    std::cin >> numSets; 
-    std::cout << "Enter number of reps: "; 
-    std::cin >> numReps; 
+    Exercises * exerciseList  = new Exercises[numExercise]; // Creating an array for exercises with dynamic memory allocation
 
-    Exercises b(nameExercise, numSets, numReps, numExercise); 
+    for (int i = 0; i < numExercise; ++i) // For loop for input on each each exercise wanted by user 
+    {
+        std::string nameExercise; // String varaible for exercise name defined 
+        int numSets, numReps; // Variables for reps and sets defined 
 
-    std::cout << "Exercise name: " << b.getExcercises() << ".\n";  
-    std::cout << "Sets completed: " << b.getSets() << ".\n"; 
-    std::cout << "Reps completed: " << b.getReps() << ".\n"; 
+        std::cout << "Enter the name of the exercise: "; // Asking user to input name of exercise 
+        std::getline(std::cin, nameExercise); // Storing user input into variable. std::getline allows for a space between words
+
+        std::cout << "Enter number of sets: "; // Asking user to input number of sets 
+        std::cin >> numSets; // Storing variable 
+
+        std::cout << "Enter number of reps: "; // Asking user to input number of reps 
+        std::cin >> numReps; // Storing variable 
+        std::cin.ignore(); // Avoid new line issues 
+
+        exerciseList[i]= Exercises(nameExercise, numSets, numReps, numExercise); // Assigning Exercises object to the i-th element in the array
+    }
+
+   std::cout << "Your exercise routine:\n"; // Displaying exercise routine to user 
+   for (int i = 0; i < numExercise; ++i)
+   {
+    std::cout << "\n"; 
+    std::cout << "Exercise name: " << exerciseList[i].getExcercises() << "\n";
+    std::cout << "Sets: " << exerciseList[i].getSets() << "\n";
+    std::cout << "Reps: " << exerciseList[i].getReps() << "\n\n";
+   }
 
     return 0; 
 
